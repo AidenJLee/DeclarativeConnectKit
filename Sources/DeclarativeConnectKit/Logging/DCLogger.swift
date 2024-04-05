ae5b23a3f7644645c8e1213f4cc99a1c7b07b2c8
hjlee <hjlee24@twayair.com>
2024-04-05 14:22:28 +0900

    fix : Header 부분 롤백
    1.0.2와 같음
    +
    공백 제거

--- Sources/DeclarativeConnectKit/Logging/DCLogger.swift
+++ Sources/DeclarativeConnectKit/Logging/DCLogger.swift
@@ -16,102 +16,102 @@
 import Foundation
 
 public enum NetworkingLogLevel {
-    case off
-    case info
-    case debug
+	case off
+	case info
+	case debug
 }
 
 class DCLogger {
-    
-    var logLevel: NetworkingLogLevel
-    
-    init(logLevel: NetworkingLogLevel = .info) {
-        self.logLevel = logLevel
-    }
-    
-    // URLRequest를 로깅하는 함수
-    func log(request: URLRequest) {
-        guard logLevel != .off else {
-            return
-        }
-        if let method = request.httpMethod, let url = request.url {
-            print("\(method) '\(url.absoluteString)'")
-            logHeaders(request)
-            logBody(request)
-        }
-        if logLevel == .debug {
-            print(request.toCurlCommand())
-        }
-    }
-    
-    // URLResponse와 Data를 로깅하는 함수
-    func log(response: URLResponse, data: Data) {
-        guard logLevel != .off else {
-            return
-        }
-        if let response = response as? HTTPURLResponse {
-            logStatusCodeAndURL(response)
-        }
-        if logLevel == .debug {
-            do {
-                let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                print(json)
-            } catch {
-                print("error : ", error.localizedDescription)
-            }
-        }
-    }
-    
-    // HTTP Header를 로깅하는 함수
-    private func logHeaders(_ urlRequest: URLRequest) {
-        if let allHTTPHeaderFields = urlRequest.allHTTPHeaderFields {
-            allHTTPHeaderFields.forEach { key, value in
-                print("\(key) : \(value)")
-            }
-        }
-    }
-    
-    // HTTP Body를 로깅하는 함수
-    private func logBody(_ urlRequest: URLRequest) {
-        if let body = urlRequest.httpBody, let str = String(data: body, encoding: .utf8) {
-            print("HttpBody : \(str)")
-        }
-    }
-    
-    // HTTP Status Code와 URL을 로깅하는 함수
-    private func logStatusCodeAndURL(_ urlResponse: HTTPURLResponse) {
-        if let url = urlResponse.url {
-            print("\(urlResponse.statusCode) '\(url.absoluteString)'")
-        }
-    }
-    
+	
+	var logLevel: NetworkingLogLevel
+	
+	init(logLevel: NetworkingLogLevel = .info) {
+		self.logLevel = logLevel
+	}
+	
+	// URLRequest를 로깅하는 함수
+	func log(request: URLRequest) {
+		guard logLevel != .off else {
+			return
+		}
+		if let method = request.httpMethod, let url = request.url {
+			print("\(method) '\(url.absoluteString)'")
+			logHeaders(request)
+			logBody(request)
+		}
+		if logLevel == .debug {
+			print(request.toCurlCommand())
+		}
+	}
+	
+	// URLResponse와 Data를 로깅하는 함수
+	func log(response: URLResponse, data: Data) {
+		guard logLevel != .off else {
+			return
+		}
+		if let response = response as? HTTPURLResponse {
+			logStatusCodeAndURL(response)
+		}
+		if logLevel == .debug {
+			do {
+				let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+				print(json)
+			} catch {
+				print("error : ", error.localizedDescription)
+			}
+		}
+	}
+	
+	// HTTP Header를 로깅하는 함수
+	private func logHeaders(_ urlRequest: URLRequest) {
+		if let allHTTPHeaderFields = urlRequest.allHTTPHeaderFields {
+			allHTTPHeaderFields.forEach { key, value in
+				print("\(key) : \(value)")
+			}
+		}
+	}
+	
+	// HTTP Body를 로깅하는 함수
+	private func logBody(_ urlRequest: URLRequest) {
+		if let body = urlRequest.httpBody, let str = String(data: body, encoding: .utf8) {
+			print("HttpBody : \(str)")
+		}
+	}
+	
+	// HTTP Status Code와 URL을 로깅하는 함수
+	private func logStatusCodeAndURL(_ urlResponse: HTTPURLResponse) {
+		if let url = urlResponse.url {
+			print("\(urlResponse.statusCode) '\(url.absoluteString)'")
+		}
+	}
+	
 }
 
 extension URLRequest {
-    
-    /**
-     Heavily inspired from : https://gist.github.com/shaps80/ba6a1e2d477af0383e8f19b87f53661d
-     */
-    
-    // URLRequest를 cURL command로 변환하는 함수
-    public func toCurlCommand() -> String {
-        guard let url: URL = url else { return "" }
-        var command: [String] = [#"curl "\#(url.absoluteString)""#]
-        
-        if let httpMethod, httpMethod != "GET", httpMethod != "HEAD" {
-            command.append("-X \(httpMethod)")
-        }
-        
-        allHTTPHeaderFields?
-            .filter { $0.key != "Cookie" }
-            .forEach { key, value in
-                command.append("-H '\(key): \(value)'")
-            }
-        
-        if let data = httpBody, let body = String(data: data, encoding: .utf8) {
-            command.append("-d '\(body)'")
-        }
-        
-        return command.joined(separator: " \\\n\t")
-    }
+	
+	/**
+	 Heavily inspired from : https://gist.github.com/shaps80/ba6a1e2d477af0383e8f19b87f53661d
+	 */
+	
+	// URLRequest를 cURL command로 변환하는 함수
+	public func toCurlCommand() -> String {
+		guard let url: URL = url else { return "" }
+		var command: [String] = [#"curl "\#(url.absoluteString)""#]
+		
+		if let httpMethod, httpMethod != "GET", httpMethod != "HEAD" {
+			command.append("-X \(httpMethod)")
+		}
+		
+		allHTTPHeaderFields?
+			.filter { $0.key != "Cookie" }
+			.forEach { key, value in
+				command.append("-H '\(key): \(value)'")
+			}
+		
+		if let data = httpBody, let body = String(data: data, encoding: .utf8) {
+			command.append("-d '\(body)'")
+		}
+		
+		return command.joined(separator: " \\\n\t")
+	}
 }
--- Sources/DeclarativeConnectKit/Module/HttpOptions.swift
+++ Sources/DeclarativeConnectKit/Module/HttpOptions.swift
@@ -13,123 +13,123 @@ public typealias HTTPHeaders = [String: String]
 
 // HTTPContentType 열거형은 String rawValue를 가지며, 각 케이스는 HTTP 요청의 Content-Type을 나타냅니다.
 public enum HTTPContentType: String {
-    case json = "application/json"
-    case urlEncoded = "application/x-www-form-urlencoded"
-    case multipart = "multipart/form-data"
+	case json = "application/json"
+	case urlEncoded = "application/x-www-form-urlencoded"
+	case multipart = "multipart/form-data"
 }
 
 // HTTPHeaderField 열거형은 String rawValue를 가지며, 각 케이스는 HTTP 요청의 헤더 필드를 나타냅니다.
 public enum HTTPHeaderField: String {
-    case authentication = "Authorization"
-    case contentType = "Content-Type"
-    case acceptType = "Accept"
-    case authToken = "X-AUTH-TOKEN"
-    case acceptEncoding = "Accept-Encoding"
+	case authentication = "Authorization"
+	case contentType = "Content-Type"
+	case acceptType = "Accept"
+	case authToken = "X-AUTH-TOKEN"
+	case acceptEncoding = "Accept-Encoding"
 }
 
 // HTTPMethod 구조체는 String rawValue를 가지며, HTTP 요청의 메소드를 나타냅니다.
 public struct HTTPMethod: RawRepresentable, Equatable, Hashable {
-    
-    public static let get = HTTPMethod(rawValue: "GET")         // `GET` 메소드.
-    public static let post = HTTPMethod(rawValue: "POST")       // `POST` 메소드.
-    public static let put = HTTPMethod(rawValue: "PUT")         // `PUT` 메소드.
-    public static let delete = HTTPMethod(rawValue: "DELETE")   // `DELETE` 메소드.
-    
-    public let rawValue: String
-
-    public init(rawValue: String) {
-        self.rawValue = rawValue
-    }
+	
+	public static let get = HTTPMethod(rawValue: "GET")         // `GET` 메소드.
+	public static let post = HTTPMethod(rawValue: "POST")       // `POST` 메소드.
+	public static let put = HTTPMethod(rawValue: "PUT")         // `PUT` 메소드.
+	public static let delete = HTTPMethod(rawValue: "DELETE")   // `DELETE` 메소드.
+	
+	public let rawValue: String
+	
+	public init(rawValue: String) {
+		self.rawValue = rawValue
+	}
 }
 
 // HttpBodyConvertible 프로토콜은 HTTP 요청의 Body를 생성하는 메소드를 가지고 있습니다.
 public protocol HttpBodyConvertible {
-    func buildHttpBodyPart(boundary: String) -> Data
+	func buildHttpBodyPart(boundary: String) -> Data
 }
 
 // MultipartData 구조체는 HTTP 요청의 Body에 포함될 멀티파트 데이터를 나타냅니다.
 public struct MultipartData {
-    let name: String
-    let fileData: Data
-    let fileName: String
-    let mimeType: String
-
-    public init(name: String, fileData: Data, fileName: String, mimeType: String) {
-        self.name = name
-        self.fileData = fileData
-        self.fileName = fileName
-        self.mimeType = mimeType
-    }
+	let name: String
+	let fileData: Data
+	let fileName: String
+	let mimeType: String
+	
+	public init(name: String, fileData: Data, fileName: String, mimeType: String) {
+		self.name = name
+		self.fileData = fileData
+		self.fileName = fileName
+		self.mimeType = mimeType
+	}
 }
 
 // HttpBodyConvertible 프로토콜을 채택한 MultipartData 구조체는 buildHttpBodyPart 메소드를 구현합니다.
 extension MultipartData: HttpBodyConvertible {
-    public func buildHttpBodyPart(boundary: String) -> Data {
-        let httpBody = NSMutableData()
-        httpBody.appendString("--\(boundary)\r\n")
-        httpBody.appendString("Content-Disposition: form-data; name=\"\(name)\"; filename=\"\(fileName)\"\r\n")
-        httpBody.appendString("Content-Type: \(mimeType)\r\n\r\n")
-        httpBody.append(fileData)
-        httpBody.appendString("\r\n")
-        return httpBody as Data
-    }
+	public func buildHttpBodyPart(boundary: String) -> Data {
+		let httpBody = NSMutableData()
+		httpBody.appendString("--\(boundary)\r\n")
+		httpBody.appendString("Content-Disposition: form-data; name=\"\(name)\"; filename=\"\(fileName)\"\r\n")
+		httpBody.appendString("Content-Type: \(mimeType)\r\n\r\n")
+		httpBody.append(fileData)
+		httpBody.appendString("\r\n")
+		return httpBody as Data
+	}
 }
 
 // Params 타입에 asPercentEncodedString 메소드를 추가합니다.
 extension Params {
-    public func asPercentEncodedString(parentKey: String? = nil) -> String {
-        return self.map { key, value in
-            var escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
-            if let `parentKey` = parentKey {
-                escapedKey = "\(parentKey)[\(escapedKey)]"
-            }
-
-            if let dict = value as? Params {
-                return dict.asPercentEncodedString(parentKey: escapedKey)
-            } else if let array = value as? [CustomStringConvertible] {
-                return array.map { entry in
-                    let escapedValue = "\(entry)"
-                        .addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
-                    return "\(escapedKey)[]=\(escapedValue)"
-                }.joined(separator: "&")
-            } else {
-                let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
-                return "\(escapedKey)=\(escapedValue)"
-            }
-        }
-        .joined(separator: "&")
-    }
+	public func asPercentEncodedString(parentKey: String? = nil) -> String {
+		return self.map { key, value in
+			var escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+			if let `parentKey` = parentKey {
+				escapedKey = "\(parentKey)[\(escapedKey)]"
+			}
+			
+			if let dict = value as? Params {
+				return dict.asPercentEncodedString(parentKey: escapedKey)
+			} else if let array = value as? [CustomStringConvertible] {
+				return array.map { entry in
+					let escapedValue = "\(entry)"
+						.addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+					return "\(escapedKey)[]=\(escapedValue)"
+				}.joined(separator: "&")
+			} else {
+				let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+				return "\(escapedKey)=\(escapedValue)"
+			}
+		}
+		.joined(separator: "&")
+	}
 }
 
 // HttpBodyConvertible 프로토콜을 채택한 Params 타입은 buildHttpBodyPart 메소드를 구현합니다.
 extension Params: HttpBodyConvertible {
-    public func buildHttpBodyPart(boundary: String) -> Data {
-        let httpBody = NSMutableData()
-        forEach { (name, value) in
-            httpBody.appendString("Content-Disposition: form-data; name=\"\(name)\"\r\n\r\n")
-            httpBody.appendString("\(value)")
-            httpBody.appendString("\r\n")
-        }
-        return httpBody as Data
-    }
+	public func buildHttpBodyPart(boundary: String) -> Data {
+		let httpBody = NSMutableData()
+		forEach { (name, value) in
+			httpBody.appendString("Content-Disposition: form-data; name=\"\(name)\"\r\n\r\n")
+			httpBody.appendString("\(value)")
+			httpBody.appendString("\r\n")
+		}
+		return httpBody as Data
+	}
 }
 
 // URL 쿼리 문자열에 포함될 수 없는 문자를 인코딩하기 위한 CharacterSet을 정의합니다.
 extension CharacterSet {
-    static let urlQueryValueAllowed: CharacterSet = {
-        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
-        let subDelimitersToEncode = "!$&'()*+,;="
-        var allowed = CharacterSet.urlQueryAllowed
-        allowed.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
-        return allowed
-    }()
+	static let urlQueryValueAllowed: CharacterSet = {
+		let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
+		let subDelimitersToEncode = "!$&'()*+,;="
+		var allowed = CharacterSet.urlQueryAllowed
+		allowed.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+		return allowed
+	}()
 }
 
 // NSMutableData에 appendString 메소드를 추가합니다.
 internal extension NSMutableData {
-    func appendString(_ string: String) {
-        if let data = string.data(using: .utf8) {
-            self.append(data)
-        }
-    }
+	func appendString(_ string: String) {
+		if let data = string.data(using: .utf8) {
+			self.append(data)
+		}
+	}
 }
